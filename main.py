76285f55e0f18df99a99f1ee80c753470c97b549
RScanlon2021 <roryscanlon21@protonmail.com>
2024-06-04 13:55:35 -0400

    converted winning logic into a function

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 def first_deal(deck):
 	return random.sample(deck, 2)
 
-
 def random_card(deck):
 	return random.choice(deck)
 
@@ -28,6 +27,16 @@ def new_game_option():
 		elif play_new_game == 'n':
 			print("Thank you for playing at Caesar's Palace!")
 			return
+		
+def winner(player_hand, computer_hand):
+	if card_totals(player_hand) > card_totals(computer_hand) and card_totals(player_hand) <= 21:
+		print("You Win!!\n")
+	elif card_totals(player_hand) == card_totals(computer_hand):
+		print("Draw\n")
+	elif card_totals(player_hand) <= 21 and card_totals(computer_hand) > 21:
+		print("You Win!!\n")
+	else:
+		print("You Lose!\n")
 
 def blackjack():
 	deck = [11,2,3,4,5,6,7,8,9,10,10,10,10]
@@ -36,7 +45,7 @@ def blackjack():
 	stand = False
 
 	print(f"Your starting hands are - Dealer:[X, {computer_hand[1]}], You:{player_hand} \n")
-	# Handle player cards
+	# Handles player cards
 	while stand == False:
 		player_input = input("\nHit or Stand?\n").lower()
 		if player_input not in ['hit', 'stand']:
@@ -57,7 +66,7 @@ def blackjack():
 	
 	print(f"Your total is {card_totals(player_hand)}\n")
 	print(computer_hand)
-	# Handle Computer cards and ace handling
+	# Handles Computer cards and ace handling
 	while True:
 		time.sleep(1)
 		if 11 in computer_hand and card_totals(computer_hand) > 21:
@@ -74,16 +83,8 @@ def blackjack():
 		
 	print(f"\nThe computer has {card_totals(computer_hand)}\n")
 	print(f"You have {card_totals(player_hand)}\n")
-	# Handle winner logic
-	if card_totals(player_hand) > card_totals(computer_hand) and card_totals(player_hand) <= 21:
-		print("You Win!!\n")
-	elif card_totals(player_hand) == card_totals(computer_hand):
-		print("Draw\n")
-	elif card_totals(player_hand) <= 21 and card_totals(computer_hand) > 21:
-		print("You Win!!\n")
-	else:
-		print("You Lose!\n")
-
+	# Handles winner logic
+	winner(player_hand, computer_hand)
 	new_game_option()
 	
 blackjack()

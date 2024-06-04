9881c4e223f5e8bd5faf920b74621c1ed720db5e
RScanlon2021 <roryscanlon21@protonmail.com>
2024-06-04 10:27:17 -0400

    Added game logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 def first_deal(deck):
 	return random.sample(deck, 2)
@@ -18,37 +19,46 @@ def card_totals(hand):
 def blackjack():
 	deck = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 	computer_hand = first_deal(deck)
-	computer_count = 0
 	player_hand = first_deal(deck)
-	player_count = 0
 	stand = False
 
-
-
-	print(f"Your starting hand is {player_hand}")
-	# print(computer_hand[0], player_hand)
+	print(f"Your starting hands are - Dealer:[X, {computer_hand[1]}], You:{player_hand} \n")
 
 	while stand == False:
-		
 		player_input = input("Hit or Stand? \n").lower()
 		if player_input not in ['hit', 'stand']:
 			print("Please choose 'Hit' or 'Stand'")
 		elif player_input == 'hit':
+			# time.sleep(1)
 			player_hand.append(random_card(deck))
 			print(player_hand)
+			if card_totals(player_hand) > 21:
+				print("You Lose!")
+				return
 		else:
 			stand = True
+	
 	print(card_totals(player_hand))
+	print(computer_hand)
 	
 	
-	
-	
-			
-	# print(computer_hand)
-	# computer_hand.append(deal_card)
-	# print(computer_hand)
-	
+	while card_totals(computer_hand) < 17:
+		# time.sleep(1)
+		computer_hand.append(random_card(deck))
+		print(computer_hand)
 
-	
 
+	print(f"\nThe computer has {card_totals(computer_hand)}\n")
+	print(f"You have {card_totals(player_hand)}\n")
+	
+	if card_totals(player_hand) > card_totals(computer_hand) and card_totals(player_hand) <= 21:
+		print("You Win!!")
+	elif card_totals(player_hand) == card_totals(computer_hand):
+		print("Draw")
+	elif card_totals(player_hand) <= 21 and card_totals(computer_hand) > 21:
+		print("You Win!!")
+	else:
+		print("You Lose!")
+	
+	
 blackjack()
